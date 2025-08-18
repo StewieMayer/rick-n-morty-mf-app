7aed37ecc9d1979848b8160972c6db6f22a35624
stewie <stewiemayer@hotmail.com>
2025-08-18 13:54:54 -0600

    Squashed 'mf-host/' content from commit fa70196
    git-subtree-dir: mf-host
    git-subtree-split: fa7019624d9c866331a2c759384729110720b7c0

---
+++ src/components/layout/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="flex justify-between items-center px-6 py-5 bg-black text-white">
+            <span>2025. Antonio Amaya</span>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/remotes/MfCharacterDetail.tsx
@@ -0,0 +1,18 @@
+import Loader from "@components/Loader";
+import { lazy, Suspense } from "react";
+
+const RemoteCharacterDetailApp = lazy(() =>
+  import("mfCharacterDetail/MfCharacterDetail").then((module) => ({
+    default: module.default,
+  }))
+);
+
+const MfCharacterDetail: React.FC = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <RemoteCharacterDetailApp />
+    </Suspense>
+  );
+};
+
+export default MfCharacterDetail;
